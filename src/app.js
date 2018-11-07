ef505429662473b84a3027203e8018fce487183e
Antohan <justanton19@gmail.com>
2018-11-07 23:04:16 +0700

    Added layout with simple form and post request to the server.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-app.set('port', 5000);
+app.set('port', port);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+
+app.post('/api/mail', (req, res) => {
+  console.log(req.body.text);
+  res.json(req.body);
 });
 
 module.exports = app;
